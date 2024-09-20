1e79494fbaeae2f37919a0b4cca7ee6c302d6e1d
alexi56 <alexdaimaman@gmail.com>
2024-09-20 16:41:23 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,20 +2,19 @@ public class Main {
 	public static void main(String[] args) {
 		
 		BankAccount lisa = new BankAccount("Lisa", 1000, -1000);
-        BankAccount bob = new BankAccount("Bob", 1000, -1000);
-
-        System.out.println(lisa);
-        System.out.println(bob);
-        
-        lisa.transfer(bob, 500);
-        
-        System.out.println(lisa);
-        System.out.println(bob);
-        
-        boolean transfer= lisa.transfer(bob, 1600);
-        
-        if(transfer==false) {
-        	System.out.println("Not enough money in account");
-        }
+	        BankAccount bob = new BankAccount("Bob", 1000, -1000);
+	
+	        System.out.println(lisa);
+	        System.out.println(bob);
+	        
+	        lisa.transfer(bob, 500);
+	        
+	        System.out.println(lisa);
+	        System.out.println(bob);
+	        
+	        boolean transfer= lisa.transfer(bob, 1600);
+	        
+	        if(transfer==false)
+	        	System.out.println("Not enough money in account");
 	}
 }
